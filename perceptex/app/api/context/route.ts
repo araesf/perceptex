3cd9c2de59c949e5ff9bdafdb8d8c619a9bad750
Ara Esfarjani <60077649+araesf@users.noreply.github.com>
2025-05-18 14:07:30 -0700

    oops

--- perceptex/app/api/context/route.ts
+++ perceptex/app/api/context/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../../generated/prisma";
+import { PrismaClient } from "../../generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 // Create a PrismaClient instances
